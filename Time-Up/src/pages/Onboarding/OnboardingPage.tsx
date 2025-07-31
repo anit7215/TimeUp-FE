c45bce63e560c76cfcc50e643f7adbe05f4ab29f
anit7215 <tina112703@gmail.com>
2025-07-31 14:56:03 +0900

    fix: ProfileSettingPage 네비게이션 시 params 타입 오류 수정

--- Time-Up/src/pages/Onboarding/OnboardingPage.tsx
+++ Time-Up/src/pages/Onboarding/OnboardingPage.tsx
@@ -11,7 +11,10 @@ export default function OnboardingPage() {
     <View className="flex-1 items-center pt-[208px] bg-black">
       <IconImage/>
       <TouchableOpacity
-        onPress={() => navigation.navigate('ProfileSettingPage')}
+        onPress={() => navigation.navigate('ProfileSettingPage', {
+          homeAddress: undefined,
+          workAddress: undefined,
+        })}
         className="w-[200px] h-[36px] bg-white rounded-full mt-[39px] px-3 py-4 justify-center border border-[#C1D5F6]">
         <View className="flex-row items-center justify-center gap-[10px]">
           <GoogleIcon width={18} height={18}/>
