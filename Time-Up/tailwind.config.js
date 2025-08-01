418b5168c9d893fa87b8db32596080c06f277ca7
taerim <shipton0201@gmail.com>
2025-08-01 16:51:33 +0900

    fix: 충돌처리

--- Time-Up/tailwind.config.js
+++ Time-Up/tailwind.config.js
@@ -24,7 +24,6 @@ module.exports = {
         'dark-stroke': '#3A3A5F',
         'gray-100': '#C9CDD1',
         'gray-200': '#CFD3D7',
-<<<<<<< HEAD
         'gray-300': '#B6BABE',
         'gray-400': '#9DA1A5', 
         'gray-500': '#84888C',
@@ -32,16 +31,6 @@ module.exports = {
         'gray-700': '#52565A',
         'gray-800': '#33373B',
         'gray-900': '#1C1F21', 
-=======
-        'gray-300': '#979B9F',
-        'gray-400': '#9DA1A5',
-        'gray-500': '#65696D',
-        'gray-600': '#6B6F73',
-        'gray-700': '#33373B',
-        'gray-750': '#52565A',
-        'gray-800': '#33373B',
-        'gray-900': '#1C1F21',
->>>>>>> b80b707072dcb2f6fc9816bb35512cd07ba69ba2
         black: '#121212',
         CalenderRed: '#E50000',
         CalenderBlue: '#224CF1',
