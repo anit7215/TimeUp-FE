d7e54d57988e1b56a3d585959eb930a25acf37f3
anit7215 <tina112703@gmail.com>
2025-08-15 05:39:28 +0900

    feat: 일기쓰기 페이지 UI

--- Time-Up/src/pages/DiaryWritePage.tsx
+++ Time-Up/src/pages/DiaryWritePage.tsx
@@ -1,15 +1,17 @@
-// src/pages/DiaryWritePage.tsx
 import React from 'react';
 import { Text, View } from 'react-native';
-import BottomLayout from '../Layouts/BottomLayout';
+import DiaryIcon from '../../assets/images/DiaryIcon.svg';
 import useAppNavigation from '../hooks/useAppNavigation';
+import BottomLayout from '../Layouts/BottomLayout';
 
 export default function DiaryWritePage() {
   const navigation = useAppNavigation();
   return (
     <BottomLayout>
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-xl text-red-600 font-bold">일기쓰기페이지</Text>
+      <View className="flex-1 items-center justify-center bg-black">
+        <DiaryIcon/>
+        <Text className="mt-[54px] text-xl text-slate-50 text-white font-medium leading-7">하루 일기 서비스를 준비중입니다!</Text>
+        <Text className="text-xl text-slate-50 text-white font-medium leading-7">곧 만나요!</Text>
       </View>
     </BottomLayout>
   );
